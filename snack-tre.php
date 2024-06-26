af3a0107b87a6537eb65caa0b2ece1f5ad38762a
Luca Sammarco <luca.sammarco7@gmail.com>
2024-06-26 18:09:07 +0200

    reformatted in html

--- snack-tre.php
+++ snack-tre.php
@@ -1,11 +1,6 @@
-<!-- Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. Stampare ogni data con i relativi post.
-Qui l’array di esempio: https://www.codepile.net/pile/R2K5d68z -->
-
-
 <?php
 
 $posts = [
-
     '10/01/2019' => [
         [
             'title' => 'Post 1',
@@ -44,15 +39,27 @@ $posts = [
     ],
 ];
 
+?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Posts</title>
+</head>
+<body>
 
-
+<?php
 foreach ($posts as $date => $dailyPosts) {
-    echo $date;
+    echo "<h2>$date</h2>";
     foreach ($dailyPosts as $post) {
-
-        echo $post['title'] . $post['author'] . $post['text'];
+        echo "<h3>{$post['title']}</h3>";
+        echo "<p>Author: {$post['author']}</p>";
+        echo "<p>{$post['text']}</p>";
     }
 }
-
 ?>
+
+</body>
+</html>
